0bf4583c9a2074a838da4652ae9bfd40fd08d041
topollonaketsana <topollomaketsana60@gmail.com>
2025-01-30 09:54:19 +0200

    update new files

--- my_profile.py
+++ my_profile.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 
 about_page = st.Page(
-    page= '/week 1/streamlit/about.py',
+    page= './week 1/streamlit/about.py',
     title= 'About Me',
-    default= True
+    default= True,
 )
 
 
@@ -29,6 +29,34 @@ st.write(f"**Field of Research:** {field}")
 st.write(f"**Institution:** {institution}")
 st.write(f"**My Research focuses on:** {research_description}")
 
+
+
+# Add a section for 
+st.header("About me")
+st.write(f'Hi, Im Topollo Naketsana, an aspiring astrophysicist currently pursuing my Honours in Astrophysics at the University of the Western Cape (UWC). I have a strong passion for exploring the mysteries of the universe, from exoplanet detection to gravitational waves and black holes. \n\nBeyond academics, I actively work on personal machine learning and astrophysics projects, some of which you can find on my https://github.com/topollonaketsana.')
+
+
+#
+
+
+
+# Add a section for Education
+st.header("Education")
+
+st.write('I hold a BSc in Physics, where I studied:\n\n\n',
+        '🔹 Mathematics (Calculus, Linear Algebra, ODEs)\n\n',
+        '🔹 Physics (Classical Mechanics, Quantum Mechanics, Astronomy)\n\n',
+        '🔹 Computational Physics (Python, Machine Learning, Scientific Computing)\n\n'
+
+)
+
+st.write('University of the western cape')
+
+
+
+#st.write()
+
+
 # Add a section for publications
 st.header("Publications")
 #uploaded_file = st.file_uploader("Upload a CSV of Publications", type="csv")
@@ -40,6 +68,9 @@ st.write( '1. Quantum computing\n\n',
 
 
 
+
+
+
 # Add a section for awards
 st.header("Awards")  
 st.write('2024:\nReceived a recognition award for attending IAU') 
@@ -47,24 +78,6 @@ st.write('2025:\nReceived a recognition award for attending CSS')
 
 
 
-# Add a section for 
-st.header("About me")
-st.write('I am Topollo Naketsana, I am currently studying my passion for the cosmos at the university of the western cape - Bsc Astrophysics..')
-
-
-#
-
-
-
-# Add a section for visualizing Education
-st.header("Education")
-
-st.write('BSc Physics \n')
-st.write('University of the western cape')
-
-
-
-#st.write()
 
 
 # Add a contact section
@@ -77,3 +90,11 @@ st.write(f"or you can contact me at {contact_number}.\n\n")
 st.write(f"This is my website {website}.\n\n")
 
 
+pg = st.navigation(
+    {
+        'info': [about_page]
+    }
+      
+
+
+)
